c911c9ab5f26ce02043b9e7c858b2d9928857a50
Adam Lenarcik <alenarcik@gmail.com>
2025-09-09 00:35:54 +0200

    added tests

---
+++ src/Debounce/Debounce/FileEvents/FileEventsDebouncer.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Subjects;
+
+namespace Debounce.FileEvents
+{
+    public class FileEventsDebouncer(
+        Publisher<FileEvent> publisher,
+        TimeSpan window) : IDisposable
+    {
+        private readonly Publisher<FileEvent> publisher = publisher;
+        private readonly Subject<FileEvent> subject = new();
+        private readonly TimeSpan window = window;
+        public IObservable<FileEvent> FileEventStream => subject;
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            subject.Dispose();
+            publisher.Dispose();
+        }
+    }
+}
---
+++ src/Debounce/Debounce/Publisher.cs
@@ -0,0 +1,42 @@
+﻿using System.Reactive.Subjects;
+
+namespace Debounce
+{
+    public class Publisher<TPublishable>(IEnumerable<TPublishable> publishables) : IDisposable
+        where TPublishable : IPublishable<TPublishable>
+    {
+        private readonly Queue<TPublishable> queue = new Queue<TPublishable>(publishables);
+        private readonly Subject<TPublishable> subject = new();
+        public IObservable<TPublishable> PublishableStream => subject;
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Dispose(true);
+        }
+
+        public async Task StartPublishingAsync(TimeSpan eventsDelay, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var item = queue.Dequeue();
+
+                var next = item.CreateNext();
+                subject.OnNext(next);
+
+                queue.Enqueue(item);
+
+                try
+                {
+                    await Task.Delay(eventsDelay, cancellationToken);
+                }
+                catch (TaskCanceledException)
+                {
+                    return;
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing) => subject.Dispose();
+    }
+}
