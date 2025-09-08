f770ea496ab2ca506a79891229b9046a2f22a40d
Adam Lenarcik <alenarcik@gmail.com>
2025-09-09 00:38:20 +0200

    added

--- src/Debounce/Debounce/FileEvents/FileEventsDebouncer.cs
+++ src/Debounce/Debounce/FileEvents/FileEventsDebouncer.cs
@@ -1,15 +1,23 @@
-﻿using System.Reactive.Subjects;
+﻿using System.Collections.Concurrent;
+using System.Reactive.Subjects;
 
 namespace Debounce.FileEvents
 {
-    public class FileEventsDebouncer(
-        Publisher<FileEvent> publisher,
-        TimeSpan window) : IDisposable
+    public class FileEventsDebouncer : IDisposable
     {
-        private readonly Publisher<FileEvent> publisher = publisher;
+        private readonly ConcurrentDictionary<string, DateTime> lastEventTimes = new();
+        private readonly Publisher<FileEvent> publisher;
         private readonly Subject<FileEvent> subject = new();
-        private readonly TimeSpan window = window;
+        private readonly IDisposable subscription;
+        private readonly TimeSpan window;
         public IObservable<FileEvent> FileEventStream => subject;
+
+        public FileEventsDebouncer(Publisher<FileEvent> publisher, TimeSpan window)
+        {
+            this.publisher = publisher;
+            this.window = window;
+            subscription = publisher.PublishableStream.Subscribe(OnNext);
+        }
 
         public void Dispose()
         {
@@ -19,8 +27,28 @@ namespace Debounce.FileEvents
 
         protected virtual void Dispose(bool disposing)
         {
-            subject.Dispose();
-            publisher.Dispose();
+            if (disposing)
+            {
+                subscription.Dispose();
+                subject.Dispose();
+                publisher.Dispose();
+            }
+        }
+
+        private void OnNext(FileEvent fileEvent)
+        {
+            var now = fileEvent.PublishTime;
+            if (lastEventTimes.TryGetValue(fileEvent.Path, out var lastTime))
+            {
+                if (now - lastTime < window)
+                {
+                    // Ignore event within window
+                    return;
+                }
+            }
+
+            lastEventTimes[fileEvent.Path] = now;
+            subject.OnNext(fileEvent);
         }
     }
 }
