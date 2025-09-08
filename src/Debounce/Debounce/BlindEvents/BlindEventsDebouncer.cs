450cbfe9478c896443450bd09901173608bf242f
Adam Lenarcik <alenarcik@gmail.com>
2025-09-09 01:10:33 +0200

    added

--- src/Debounce/Debounce/BlindEvents/BlindEventsDebouncer.cs
+++ src/Debounce/Debounce/BlindEvents/BlindEventsDebouncer.cs
@@ -5,13 +5,19 @@ namespace Debounce.BlindEvents
 {
     public class BlindEventsDebouncer : IDisposable
     {
+        private readonly int maxHeight;
+        private readonly int minHeight;
+        private readonly TimeSpan moveTimeout;
         private readonly Subject<BlindEvent> subject = new();
         private readonly IDisposable subscription;
         public IObservable<BlindEvent> EventStream => subject;
 
-        public BlindEventsDebouncer(Publisher<BlindEvent> publisher)
+        public BlindEventsDebouncer(Publisher<BlindEvent> publisher, int minHeight, int maxHeight, TimeSpan moveTimeout)
         {
             subscription = publisher.PublishableStream.Subscribe(OnNext);
+            this.minHeight = minHeight;
+            this.maxHeight = maxHeight;
+            this.moveTimeout = moveTimeout;
         }
 
         public void Dispose()
