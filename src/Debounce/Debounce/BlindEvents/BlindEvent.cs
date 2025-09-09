f7dd0577d2033102d44f96bb042d96ea0c586fe5
Adam Lenarcik <alenarcik@gmail.com>
2025-09-09 07:52:11 +0200

    added

--- src/Debounce/Debounce/BlindEvents/BlindEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Debounce.BlindEvents
-{
-    public record BlindEvent(int Position, DateTime PublishTime) : IPublishable<BlindEvent>
-    {
-        public BlindEvent CreateNext() => this with { PublishTime = DateTime.UtcNow };
-        public static BlindEvent Create(int position) => new(position, DateTime.MinValue);
-    }
-}
